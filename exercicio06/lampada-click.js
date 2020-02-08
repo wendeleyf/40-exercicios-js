cc5b0ff4698932ec67d90df49b8c845c737c74f5
wendeleyf <wendeleyti@gmail.com>
2020-02-08 08:07:45 -0300

    Add exercicio06

---
+++ exercicio06/lampada-click.js
@@ -0,0 +1,3 @@
+document.getElementById("lampada-off").addEventListener("click", function(){
+    document.getElementById("lampada-off").src = "https://github.com/gabrieldarezzo/helpjs-ravi/blob/master/images/lampada-on.jpg?raw=true";
+})
