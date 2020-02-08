785ee621c7e036b69b676866c6daa0af288ce57e
wendeleyf <wendeleyti@gmail.com>
2020-02-08 08:28:41 -0300

    Add exercicio07

---
+++ exercicio07/lampada-mouse.js
@@ -0,0 +1,3 @@
+document.getElementById("lampada").addEventListener("mouseover", function(){
+  document.getElementById("lampada").src = "https://github.com/gabrieldarezzo/helpjs-ravi/blob/master/images/lampada-on.jpg?raw=true";
+})
