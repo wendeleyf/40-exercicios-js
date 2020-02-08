f6b43d6e465b9364ecff377c1412c4430979d76d
wendeleyf <wendeleyti@gmail.com>
2020-02-08 10:57:09 -0300

    Add exercicio12a

---
+++ exercicio12a/cores.js
@@ -0,0 +1,7 @@
+document.querySelector("#verde").addEventListener("click", function(){
+  document.querySelector("#quadrado").style = "background:green;";
+})
+
+document.querySelector("#vermelho").addEventListener("click", function(){
+  document.querySelector("#quadrado").style = "background:red;";
+})
